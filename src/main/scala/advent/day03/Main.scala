0aa7db58c7ccf1b7a04d68fc7e1e21af25310beb
Ludovic Guegan <ludovic.guegan@gmail.com>
2018-12-07 16:46:12 +0100

    day03 part2

--- src/main/scala/advent/day03/Main.scala
+++ src/main/scala/advent/day03/Main.scala
@@ -4,13 +4,11 @@ import scala.io.Source
 object Main extends App {
   val source = Source.fromResource("day03.txt").getLines.toList
   // #1 @ 146,196: 19x14
+  case class Point(x: Int, y: Int)
   case class Claim(id: Int, l: Int, t: Int, w: Int, h: Int) {
-    def contains(x: Int, y: Int): Boolean = {
-      (x >= l && x <= l + w) && (y >= t && y <= t + h)
-    }
-    def emits: Seq[Tuple2[Int,Int]] = {
+    def emits: Seq[Point] = {
       for (x <- l until l+w;
-           y <- t until t+h) yield(x,y)
+           y <- t until t+h) yield Point(x,y)
     }
   }
   def parse(str: String): Claim = {
@@ -20,9 +18,11 @@ object Main extends App {
   }
   val input = source.map(parse(_))
 
-  val result = input.flatMap(_.emits).groupBy(a => a).count{
-    case (a, l) => l.size >= 2
-  }
+  val groupped = input.flatMap(claim => claim.emits.map(Tuple2(claim, _))).groupBy(a => a._2)
+  val dups = groupped.flatMap{ case (p, list) =>
+    if (list.size >= 2) list.map(_._1) else List()
+  }.toSeq.distinct
+  val result = input.find(claim => !dups.contains(claim))
   println(result)
 }
 
