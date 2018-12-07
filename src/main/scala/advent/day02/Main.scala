55bb42502518cd311d6fb615dbb6c940500ebe27
Ludovic Guegan <ludovic.guegan@gmail.com>
2018-12-07 13:28:32 +0100

    day02 part2

--- src/main/scala/advent/day02/Main.scala
+++ src/main/scala/advent/day02/Main.scala
@@ -10,11 +10,17 @@ object Main extends App {
     return list.distinct.count(c => list.count(_ == c) == n)
   }
 
-  def has(list: Seq[Char], n: Int): Int = {
-    if (count(list, n) == 0) return 0 else return 1
+  def has(list: Seq[Char], n: Int): Boolean = !(count(list, n) == 0)
+
+  def diff(l1: Seq[Char], l2: Seq[Char]): Int = {
+    l1.zip(l2).map({ case (a, b) => a == b}).count(_ == false)
+  }
+  def common(l1: Seq[Char], l2: Seq[Char]): Seq[Char] = {
+    l1.zip(l2).flatMap({ case (a, b) => if (a == b) List(a) else List() })
   }
 
-  val twos = input.map(has(_, 2)).reduce(_ + _)
-  val threes = input.map(has(_, 3)).reduce(_ + _)
-  println(twos * threes)
+  val fabrics = input.filter(l => has(l, 2) || has(l, 3))
+  val result = fabrics.flatMap(l1 => fabrics.filter(l2 => diff(l1, l2) == 1))
+  result.foreach(println(_))
+  println(common(result(0), result(1)).mkString)
 }
